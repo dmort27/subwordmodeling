9751a1aee26149adcc9a71d67da49f316a1f48dc
David R. Mortensen <dmortens@cs.cmu.edu>
2023-05-17 20:16:06 -0400

    Reversion

--- docs/install_packages.sh
+++
@@ -1,9 +0,0 @@
-#conda create -n bf550 python=3.9
-#conda activate bf550
-conda install -y -c conda-forge mamba
-mamba install -y -c conda-forge python=3 ruby=2.7.2 gxx_linux-64 numpy libcairo2
-pip install jupyterlab pandas matplotlib drawSvg
-
-gem install bundler
-
-bundle install
